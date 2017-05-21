f37c86aa53876057716c34d4fe053a44653121c2
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2017-05-21 17:07:40 +0200

    Use a List

--- PreAndAppenderStrategies/Program.cs
+++ PreAndAppenderStrategies/Program.cs
@@ -10,55 +10,27 @@ namespace PreAndAppenderStrategies
     {
         static void Main(string[] args)
         {
-            var queue = new System.Collections.Generic.Queue<string>();
+            var list = new System.Collections.Generic.List<string>();
 
-            queue.Enqueue("first in");
-            //queue.Enqueue(3); // not allowed anymore
-            queue.Enqueue("first out");
-            queue.Enqueue("FIFO");
+            list.Add("FIFO, LIFO, FILO or LILO?"); // Enqueue
+            list.Add("With a List or array ..."); // Enqueue
+            list.Add("... we can choose!"); // Enqueue
 
-            Console.WriteLine(queue.Peek()); // we only peek
-            Console.WriteLine(queue.Count); // all are still in
-            Console.WriteLine(queue.Dequeue()); // we take out "LIFO"
-            Console.WriteLine(queue.Dequeue()); // we take out "first out"
-            Console.WriteLine(queue.Dequeue());
-            Console.WriteLine(queue.Count); // stack is empty
+            // do a 'Peek'
+            Console.WriteLine(list[0]);
 
-            try
-            {
-                Console.WriteLine(queue.Dequeue());
-            }
-            catch(InvalidOperationException e) {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(e.GetType());
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("Data: "); Console.ResetColor();
-                Console.WriteLine(e.Data);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("HelpLink: ");Console.ResetColor();
-                Console.WriteLine(e.HelpLink);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("HResult: ");Console.ResetColor();
-                Console.WriteLine(e.HResult);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("Message: ");Console.ResetColor();
-                Console.WriteLine(e.Message);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("Source: ");Console.ResetColor();
-                Console.WriteLine(e.Source);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("StackTrace: ");Console.ResetColor();
-                Console.WriteLine(e.StackTrace);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("TargetSite: ");Console.ResetColor();
-                Console.WriteLine(e.TargetSite);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("ToString: ");Console.ResetColor();
-                Console.WriteLine(e.ToString());
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("END " + e.GetType());
-            }
+            // count
+            Console.WriteLine(list.Count);
 
+            // do a 'Dequeue'
+            Console.WriteLine(list[list.Count - 1]);
+            list.RemoveAt(list.Count - 1);
+
+            // do a 'Pop'
+            Console.WriteLine(list[0]);
+            list.RemoveAt(0);
+
+            Console.WriteLine(list.Count); // 1 left
         }
     }
 }
--- README.md
+++ README.md
@@ -6,7 +6,12 @@ Dit is een inleiding in **datastructuren** en de bijhorende **algoritmen**.
 
 - Gebruik van een `System.Collections.Stack` + analyse van de exception bij aanroepen van `Pop()` bij een lege Stack.
 - Gebruik van een `System.Collections.Queue` + exception bij aanroepen van `Dequeue()` bij een lege Queue.
+- Gebruik van een `System.Collection.Generic.Queue<T>` met `T` als `string`
 
 # In deze branch
 
-- Gebruik van een `System.Collection.Generic.Queue<T>` met `T` als `string`
+- Gebruik van een `System.Collections.Generic.List<T>`
+- Exception handling verwijderd voor de duidelijkheid
+- Demonstratie van de concepten `Push` en `Pop` (Stack) en `Enqueue` en `Dequeue` (Queue) met een `List<T>` als achterliggende datastructuur
+
+
